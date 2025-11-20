4888108dd4d3be6205c65272733a51a2b429d8d1
plexescor <saraswal2010@gmail.com>
2025-11-20 20:40:27 +0530

    HI

---
+++ Scripts/WindowTrackerAndShit/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Headers/test.h"
+
+void sayHello()
+{
+    std::cout << "Hello World \n";
+}
+
+int add(int a, int b)
+{
+    return a + b;
+}
---
+++ Scripts/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "WindowTrackerAndShit/Headers/test.h"
+
+int main()
+{
+    sayHello();
+    std::cout << add(5,9);
+}
